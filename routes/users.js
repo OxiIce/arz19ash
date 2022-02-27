81fb93e475c886c3d5ac5cb1add2e5741d34e308
OxiIce <theoxiice@gmail.com>
2022-02-27 20:02:54 +0200

    Refactoring code

--- routes/users.js
+++ routes/users.js
@@ -5,7 +5,7 @@ const {User} = require('../models/users')
 
 // POST: CREATE A NEW USER
 router.post('/', (req,res) => {
-
+    
     user = new User({
         name:req.body.userName,
         timeOnPosts:req.body.timeOnPosts,
@@ -27,30 +27,6 @@ router.get('/',(req,res) => {
     })
 })
 
-// GET THE USER BY ID
-// router.get('/:userId', (req,res) => {
-//     User.findById(req.params.userId).then(user => {
-//         if(user) res.send(user);
-//         res.status(404).send("User not found")
-//     }).catch(err => {
-//         // res.status(500).send(err.message)
-//         res.status(500).send("User NOT found")
-//     })
-// })
-
-// UPDATE USER BASED ON ID
-// router.put("/:userId", async (req,res) => {
-//     const updatedUser = await User.findByIdAndUpdate(req.params.userId, {
-//         name:req.body.userName,
-//         timeOnPosts:req.body.timeOnPosts,
-//         numberOfLicenses:req.body.numberOfLicenses
-//     },
-//     {new:true})
-
-//     if(!updatedUser) res.status(404).send("User not foundd");
-//     res.send(updatedUser)
-// })
-
 // DELETE USER BASED ON ID
 router.delete("/:userId", async (req, res) => {
     const user = await User.findByIdAndRemove(req.params.userId)
@@ -58,9 +34,6 @@ router.delete("/:userId", async (req, res) => {
     if(!user) res.status(404).send("User with id not found")
     res.send(user)
 })
-
-
-
 
 // GET USER BY USER NAME
 router.get('/:userName', (req,res) => {
@@ -77,13 +50,6 @@ router.get('/:userName', (req,res) => {
     })
 })
 
-
-
-
-
-
-
-
 // UPDATE USER BASED ON ID
 router.put("/:userId", async (req,res) => {
     
@@ -97,47 +63,5 @@ router.put("/:userId", async (req,res) => {
     if(!updatedUser) res.status(404).send("User not foundd");
     res.send(updatedUser)
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// UPDATE USER BASED ON USER NAME
-// router.put("/:userName", async (req,res) => {
-
-//     User.find({name: req.params.userName}).then(user => {
-//         if(user.length > 0) {
-//             const userId = user[0]._id
-
-            
-//             const updatedUser = User.findByIdAndUpdate(userId, {
-//                 name:req.body.userName,
-//                 timeOnPosts:req.body.timeOnPosts,
-//                 numberOfLicenses:req.body.numberOfLicenses
-//             },
-//             {new:true})
-//             if(!updatedUser) res.status(404).send("User not foundd");
-//             res.send(updatedUser)
-            
-
-//         } else {
-//             res.status(404).send("No user with this name");
-//         }
-//     })
-//     .catch((err) => {
-//         res.status(500).send("Something went wrong");
-//     })
-// })
 
 module.exports = router;
