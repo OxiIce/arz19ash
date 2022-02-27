c0722f0620146bdc112acf79a3d75426b8244126
OxiIce <theoxiice@gmail.com>
2022-02-27 19:47:02 +0200

    Added router related to seaching users by name

--- models/users.js
+++ models/users.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+// const yup = require('yup')
 
 // USER SCHEMA
 const UserSchema = new mongoose.Schema({
@@ -22,4 +23,19 @@ const UserSchema = new mongoose.Schema({
     }
 });
 
-module.exports = new mongoose.model('Users', UserSchema)
+// const validataUser = user => {
+//     const schema = yup.object().shape({
+//         userName:yup.string().required().min(3).max(50),
+//         timeOnPosts:yup.number().required().min(0).max(999999),
+//         numberOfLicenses:yup.number().required().min(0).max(999999)
+//     });
+
+//     return schema.validate(user).then(user => user).catch(err => {
+//         return {
+//             message:error.message
+//         }
+//     })
+// }
+
+exports.User = new mongoose.model('User', UserSchema)
+// exports.validataUser = validataUser;
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/users')
+// const {User, validataUser} = require('../models/users')
+const {User} = require('../models/users')
 
 // POST: CREATE A NEW USER
-router.post('/',(req,res) => {
-    
+router.post('/', (req,res) => {
+
     user = new User({
         name:req.body.userName,
         timeOnPosts:req.body.timeOnPosts,
@@ -16,6 +17,127 @@ router.post('/',(req,res) => {
     }).catch(err => {
         res.status(500).send("User was not stored in DB")
     })
+});
+
+// GET ALL USERS
+router.get('/',(req,res) => {
+    User.find().then(users => res.send(users))
+    .catch((err) => {
+        res.status(500).send("Something went wrong");
+    })
 })
+
+// GET THE USER BY ID
+// router.get('/:userId', (req,res) => {
+//     User.findById(req.params.userId).then(user => {
+//         if(user) res.send(user);
+//         res.status(404).send("User not found")
+//     }).catch(err => {
+//         // res.status(500).send(err.message)
+//         res.status(500).send("User NOT found")
+//     })
+// })
+
+// UPDATE USER BASED ON ID
+// router.put("/:userId", async (req,res) => {
+//     const updatedUser = await User.findByIdAndUpdate(req.params.userId, {
+//         name:req.body.userName,
+//         timeOnPosts:req.body.timeOnPosts,
+//         numberOfLicenses:req.body.numberOfLicenses
+//     },
+//     {new:true})
+
+//     if(!updatedUser) res.status(404).send("User not foundd");
+//     res.send(updatedUser)
+// })
+
+// DELETE USER BASED ON ID
+router.delete("/:userId", async (req, res) => {
+    const user = await User.findByIdAndRemove(req.params.userId)
+
+    if(!user) res.status(404).send("User with id not found")
+    res.send(user)
+})
+
+
+
+
+// GET USER BY USER NAME
+router.get('/:userName', (req,res) => {
+    User.find({name: req.params.userName}).then(user => {
+        if(user.length > 0) {
+            // res.send(user)
+            res.send(user[0]._id)
+        } else {
+            res.status(404).send("No user with this name");
+        }
+    })
+    .catch((err) => {
+        res.status(500).send("Something went wrong");
+    })
+})
+
+
+
+
+
+
+
+
+// UPDATE USER BASED ON ID
+router.put("/:userId", async (req,res) => {
+    
+    const updatedUser = await User.findByIdAndUpdate(req.params.userId, {
+        name:req.body.userName,
+        timeOnPosts:req.body.timeOnPosts,
+        numberOfLicenses:req.body.numberOfLicenses
+    },
+    {new:true})
+
+    if(!updatedUser) res.status(404).send("User not foundd");
+    res.send(updatedUser)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// UPDATE USER BASED ON USER NAME
+// router.put("/:userName", async (req,res) => {
+
+//     User.find({name: req.params.userName}).then(user => {
+//         if(user.length > 0) {
+//             const userId = user[0]._id
+
+            
+//             const updatedUser = User.findByIdAndUpdate(userId, {
+//                 name:req.body.userName,
+//                 timeOnPosts:req.body.timeOnPosts,
+//                 numberOfLicenses:req.body.numberOfLicenses
+//             },
+//             {new:true})
+//             if(!updatedUser) res.status(404).send("User not foundd");
+//             res.send(updatedUser)
+            
+
+//         } else {
+//             res.status(404).send("No user with this name");
+//         }
+//     })
+//     .catch((err) => {
+//         res.status(500).send("Something went wrong");
+//     })
+// })
 
 module.exports = router;
